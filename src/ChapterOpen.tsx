24685c87c919527355c61ae67802c7e828f514d4
rxliuli <rxliuli@gmail.com>
2022-06-24 16:15:19 +0800

    feat: 实现 title 部分

--- src/ChapterOpen.tsx
+++ src/ChapterOpen.tsx
@@ -3,32 +3,51 @@ import './styles/chapter-open.less'
 export function ChapterOpen() {
   return (
     <div class={'chapter-open'}>
-      <div class={'bg'}>
+      {/* <div class={'bg'}>
         <div class={'bg1'}>
-          <span />
-          <span />
+          <span></span>
+          <span></span>
         </div>
         <div class={'bg2'}>
-          <span />
-          <span />
+          <span></span>
+          <span></span>
+        </div>
+      </div> */}
+      {/* <div class={'lines'}>
+        <div class={'top'}></div>
+        <div class={'bottom'}></div>
+      </div> */}
+      {/* <div class={'ball'}>
+        <span></span>
+        <span></span>
+      </div> */}
+      <div class={'title'}>
+        <div class={'item-box'}>
+          <div class={'item'}>
+            <div class={'mo'}></div>
+          </div>
+          <div class={'item'}>
+            <div class={'fa'}></div>
+            <div class={'nv'}></div>
+          </div>
         </div>
       </div>
       {/* <div class={'switch'}>
         <div class={'balls'}>
-          <span />
-          <span />
+          <span></span>
+          <span></span>
         </div>
         <div class={'balls'}>
-          <span />
-          <span />
+          <span></span>
+          <span></span>
         </div>
         <div class={'balls'}>
-          <span />
-          <span />
+          <span></span>
+          <span></span>
         </div>
         <div class={'balls'}>
-          <span />
-          <span />
+          <span></span>
+          <span></span>
         </div>
         <div class={'rotate'}></div>
       </div> */}
