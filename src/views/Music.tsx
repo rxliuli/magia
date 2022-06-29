b9d6b7c8fd575bf8bd79be6114ee04d8ea17772d
rxliuli <rxliuli@gmail.com>
2022-06-29 10:24:43 +0800

    feat: 完成音乐播放器部分

--- src/views/Music.tsx
+++ src/views/Music.tsx
@@ -1,3 +1,24 @@
+import { useReducer, useRef } from 'preact/hooks'
+import '../styles/music.less'
+
 export function Music() {
-  return <div></div>
+  const audioRef = useRef<HTMLAudioElement>(null)
+  const [state, next] = useReducer<'play' | 'pause', void>((s) => {
+    const next = s === 'pause' ? 'play' : 'pause'
+    if (next === 'play') {
+      audioRef.current!.play()
+    } else {
+      audioRef.current!.pause()
+    }
+    return next
+  }, 'pause') as ['play' | 'pause', () => void]
+  return (
+    <div class={`music ${state}`} onClick={next}>
+      <audio
+        class={'hide'}
+        ref={audioRef}
+        src={'https://cdn.jsdelivr.net/npm/cyris-music-cdn/Kalafina_Magia.mp3'}
+      ></audio>
+    </div>
+  )
 }
